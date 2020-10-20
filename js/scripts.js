08d5f5f28f14d61e2193986011d5802e3f4b1a50
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-10-20 13:26:12 -0700

    add js files and add color theme
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com> Eric Stratton <strattonericj@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("button#dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+
+  $("button#light").click(function(){
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+
+  $(".highlight").click(function() {
+    $(".highlight").toggleClass("pHighlight");
+  });
+
+});
